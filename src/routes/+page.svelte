ede5bfd8aabbe10e38c02db7badd1082c5b270b6
kenjlee09@gmail.com <kenjlee09@gmail.com>
2025-05-30 12:56:47 -0700

    a little work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
+
+
+
+<h1>hi</h1>
+<a href="/about">About my site</a>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+
+<style>
+    @import "open-props/style"
+    @import "open-props/normalize"
+    @import "open-props/typography"
+</style>
